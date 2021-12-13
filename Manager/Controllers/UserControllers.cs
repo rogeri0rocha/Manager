7f2dbae9973d1094b304771ea8d1f56c32e4970c
rogerio Rocha <roger@DESKTOP-RVHNPJ7>
2021-12-12 22:54:13 -0300

    Construção da controller

---
+++ Manager/Controllers/UserControllers.cs
@@ -0,0 +1,33 @@
+﻿using Manage.Core.Exceptions;
+using Manager.ViewsModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Manager.Controllers
+{
+
+[ApiController]
+    public class UserControllers : ControllerBase
+    {
+        [HttpPost]
+        [Route("/api/v1/user/create")]
+        public async Task<IActionResult> Create([FromBody]CreateUserViewModel createUserViewModel)
+        {
+            try
+            {
+                return Ok();
+            }
+
+            catch (DomainException ex)
+            { 
+                return BadRequest(ex.Message);
+            }
+            catch(Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+    }
+}
---
+++ Manager/ViewsModel/CreateUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Manager.ViewsModel
+{
+    public class CreateUserViewModel
+    {
+        [Required(ErrorMessage ="O nome não pode ser vazio")]
+        [MinLength(3,ErrorMessage = "Digite um nome valido")]
+        [MaxLength(80, ErrorMessage = "Digite um nome valido")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "O email não pode ser vazio")]
+        [MinLength(10, ErrorMessage = "Digite um email valido")]
+        [MaxLength(40, ErrorMessage = "Digite um email valido")]
+        [RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha não pode ser vazia")]
+        [MinLength(8, ErrorMessage = "Digite uma senha valida")]
+        [MaxLength(20, ErrorMessage = "Digite uma senha valida")]
+        public string Password { get; set; }
+    }
+}
