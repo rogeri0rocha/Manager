0bd39e8cb5626966ad4d4bbafc6eedd949a8ebc1
rogerio Rocha <roger@DESKTOP-RVHNPJ7>
2021-12-19 00:23:01 -0300

    Adicionando dbcontext

--- Manager/Startup.cs
+++ Manager/Startup.cs
@@ -1,8 +1,19 @@
+using AutoMapper;
+using Manager.Domain.Entities;
+using Manager.Infra.Context;
+using Manager.Infra.Interface;
+using Manager.Infra.Repository;
+using Manager.services.DTO;
+using Manager.services.Interfaces;
+using Manager.Services.Services;
+using Manager.ViewsModel;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Manager
@@ -19,11 +30,27 @@ namespace Manager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var autoMapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserDTO>().ReverseMap();
+                cfg.CreateMap<CreateUserViewModel, UserDTO>().ReverseMap();
+          });
+
+            services.AddSingleton(autoMapperConfig.CreateMapper());
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
             services.AddMvc();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "1", Version = "v1" });
             });
+
+            services.AddDbContext<ManagerContext>(options => options
+               .UseSqlServer(Configuration["ConnectionStrings:ManagerAPISqlServer"])
+               .EnableSensitiveDataLogging()
+               .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole())),
+           ServiceLifetime.Transient);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
